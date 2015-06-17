c3c15eb799f9488de07998922f2baedcb3d44896
Joel Ferman <joelferman@hotmail.com>
2015-06-16 20:49:32 -0600

    Counts to 0...

--- app/src/main/java/yaxstudio/com/fermanexport/PackageDetailsDeliveryTeamActivity.java
+++ app/src/main/java/yaxstudio/com/fermanexport/PackageDetailsDeliveryTeamActivity.java
@@ -1,38 +1,24 @@
 package yaxstudio.com.fermanexport;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
-public class PackageDetailsDeliveryTeamActivity extends ActionBarActivity {
+public class PackageDetailsDeliveryTeamActivity extends Activity implements OnClickListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package_details_delivery_team);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_package_details_delivery_team, menu);
-        return true;
-    }
+    public void onClick(View v)
+    {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
